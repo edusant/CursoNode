bd1af788dee7ab3a5634030558f74abbfb28c086
Eduardo Santos Silva <eduardos_silva@hotmail.com>
2020-03-29 17:12:57 -0300

    recomeçar

--- aula 1/app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-
-http.createServer(function(req, res){
-	res.end("Ola")
-}).listen(8080);
-console.log("Servidor de boa")
--- teste.sql
+++
@@ -1,5 +0,0 @@
-create table usuarios(
-	nome varchar(50),
-	email varchar(100),
-	idade int
-)
